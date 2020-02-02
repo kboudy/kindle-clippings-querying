785acfe753fc761226d086fb2d501c4abb2b51a6
Keith Boudreau <kboudy@gmail.com>
2020-02-02 15:45:45 -0500

    coverting to epub, but working out issues

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const readline = require("readline"),
   fs = require("fs"),
   path = require("path"),
   chalk = require("chalk"),
+  util = require("util"),
+  exec = util.promisify(require("child_process").exec),
   homedir = require("os").homedir(),
   _ = require("lodash"),
   moment = require("moment");
@@ -37,14 +39,35 @@ const getEmptyClip = () => {
   };
 };
 
-const myClippingPath = path.join(
+const configPath = path.join(
   homedir,
-  "Dropbox/app_config/kindle-clippings-querying/My Clippings.txt"
+  ".config/kindle-clippings-querying/config.json"
 );
+if (!fs.existsSync(path.dirname(configPath))) {
+  fs.mkdirSync(path.dirname(configPath));
+}
+let config = {
+  // converted epub files will be stored in this dir
+  epubOutputDir: "/data/kindleClippings_epubFiles",
+  // "My Clippings.txt" will be copied from the kindle to this location
+  myClippingsLocalPath: path.join(
+    homedir,
+    "Dropbox/app_config/kindle-clippings-querying/My Clippings.txt"
+  ),
+  kindleDocumentsDir: `/media/${
+    require("os").userInfo().username
+  }/Kindle/Documents`
+};
+if (!fs.existsSync(configPath)) {
+  fs.writeFileSync(configPath, JSON.stringify(config), "utf8");
+} else {
+  config = JSON.parse(fs.readFileSync(configPath));
+}
 
-const kindlePath = `/media/${
-  require("os").userInfo().username
-}/Kindle/Documents/My Clippings.txt`;
+const kindleClippingsPath = path.join(
+  config.kindleDocumentsDir,
+  "My Clippings.txt"
+);
 
 const argOptions = {
   fields: {
@@ -116,7 +139,8 @@ const writeCompletionFile = () => {
   }
 };
 
-const processLineByLine = async function processLineByLine(filePath) {
+const processLineByLine = async filePath => {
+  const allTitles = [];
   const rli = readline.createInterface({
     input: fs.createReadStream(filePath),
     crlfDelay: Infinity
@@ -153,6 +177,10 @@ const processLineByLine = async function processLineByLine(filePath) {
       const matches = line.match(regExExtractTitleAndAuthor);
       currentClipping.bookTitle = matches[1].trim();
       currentClipping.author = matches[3].trim();
+
+      if (!allTitles.includes(currentClipping.bookTitle)) {
+        allTitles.push(currentClipping.bookTitle);
+      }
     } else if (currentClipping.pageStart === null) {
       const rxLocationAndDate = /^.* Location (.*?) \| Added on (.*)$/;
       const matches = line.match(rxLocationAndDate);
@@ -249,34 +277,110 @@ const processLineByLine = async function processLineByLine(filePath) {
     }
     console.log(outString);
   }
+  return allTitles;
+};
+
+const alphaNumericOnly = text => {
+  return text.replace(/[^a-z0-9]+/gi, "");
+};
+
+const findMatchingFile = (bookTitle, ebookFiles) => {
+  const strippedBookTitle = alphaNumericOnly(bookTitle)
+    .slice(0, 100)
+    .toLowerCase();
+  const matches = ebookFiles.filter(b => {
+    if (!b.includes("_")) {
+      return false;
+    }
+    const bCropped = b.substring(0, b.lastIndexOf("_"));
+    const bStripped = alphaNumericOnly(bCropped)
+      .slice(0, 100)
+      .toLowerCase();
+    return bStripped === strippedBookTitle;
+  });
+  if (matches.length) {
+    return matches[0];
+  }
+  return null;
+};
+
+const executeAll = async () => {
+  console.log(
+    `Copying ${chalk.yellow(kindleClippingsPath)} to ${chalk.green(
+      config.myClippingsLocalPath
+    )}`
+  );
+  fs.copyFileSync(kindleClippingsPath, config.myClippingsLocalPath);
+
+  console.log("Importing clippings...");
+  let allTitles = await processLineByLine(config.myClippingsLocalPath).catch(
+    console.error
+  );
+  /*
+  allTitles.sort();
+  console.log("Copying & converting ebooks...");
+  const allDocFiles = fs.readdirSync(config.kindleDocumentsDir);
+  const azwFiles = allDocFiles.filter(f => f.match(/\.azw$/));
+  const missingTitles = [];
+  for (const t of allTitles) {
+    const m = findMatchingFile(t, azwFiles);
+    if (m) {
+      const mFullPath = path.join(config.kindleDocumentsDir, m);
+      const destEpubFile = path.join(config.epubOutputDir, `${t}.epub`);
+      if (!fs.existsSync(destEpubFile)) {
+        console.log(
+          `Converting ${chalk.yellow(t)} - output file: ${chalk.green(
+            destEpubFile
+          )}`
+        );
+
+        try {
+          const { stdout, stderr } = await exec(
+            `ebook-convert "${mFullPath}" "${destEpubFile}"`
+          );
+        } catch (err) {
+          console.log(err.message);
+        }
+      }
+    } else {
+      missingTitles.push(t);
+    }
+  }
+  */
 };
 
 const debugging =
   typeof v8debug === "object" ||
   /--debug|--inspect/.test(process.execArgv.join(" "));
 if (debugging) {
-  processLineByLine(myClippingPath).catch(console.error);
+  console.log("Debugger session");
+  (async () => {
+    await executeAll();
+  })();
 }
-
 writeCompletionFile();
 
 module.exports = () => {
   if (argv.copy) {
-    if (!fs.existsSync(myClippingPath)) {
+    if (!fs.existsSync(config.myClippingsLocalPath)) {
       console.log(
         chalk.red(
           `Couldn't find the Kindle clipping file (make sure you've mounted it by running nautilus): ${chalk.white(
-            myClippingPath
+            config.myClippingsLocalPath
           )}`
         )
       );
     } else {
-      fs.copyFileSync(myClippingPath, kindlePath);
       console.log(
-        `Copied ${chalk.yellow(myClippingPath)} to ${chalk.green(kindlePath)}`
+        `Copying ${chalk.yellow(kindleClippingsPath)} to ${chalk.green(
+          config.myClippingsLocalPath
+        )}`
       );
+      fs.copyFileSync(kindleClippingsPath, config.myClippingsLocalPath);
     }
   } else {
-    processLineByLine(myClippingPath).catch(console.error);
+    (async () => {
+      await executeAll();
+    })();
   }
 };
